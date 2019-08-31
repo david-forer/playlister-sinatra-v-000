42905f890237f9b8afe38d0dc6b71606983aad22
David Forer <david.j.forer@gmail.com>
2019-08-31 06:40:53 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,14 +1,14 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :song_genres
-  has_many :genres, :through => :songs
-  
+  has_many :genres, through: :songs
+
   def slug
-   name.downcase.gsub(" ","-")
- end
-  
-  def self.find_by_slug(slug)
-   Artist.all.find{|artist| artist.slug == slug}
+    name = self.name.downcase.gsub(' ','-')
   end
-  
+
+  def self.find_by_slug(slug)
+    Artist.all.find do |artist|
+      artist.slug == slug
+    end
+  end
 end
